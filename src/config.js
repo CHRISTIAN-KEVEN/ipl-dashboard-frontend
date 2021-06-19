27f51de97249c13777bfe181275297e39593a6a5
AJEITOH <christiankeven326@gmail.com>
2021-06-19 13:26:25 +0100

    Adds team and match page with data

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@
 const baseUrl = 'http://localhost:8080';
 
 const API = {
-    team: (teamName) => `${baseUrl}/teams/${teamName}`
+    team: (teamName) => `${baseUrl}/teams/${teamName}`,
+    matches: (teamName, year) => `${baseUrl}/teams/${teamName}/matches?year=${year}`
 };
 
 export default API
--- src/pages/Match/MatchPage.jsx
+++ src/pages/Match/MatchPage.jsx
@@ -1,12 +1,37 @@
 
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import API from '../../config';
+import MatchDetailsCard from '../Teams/components/Match-Details-Card';
 
 const MatchPage = () => {
 
+    const [matches, setMatches] = useState();
+    const {teamName, year} = useParams();
+    console.log('Params', useParams())
+    useEffect(() => {
+
+        const fetchMatch = async () => {
+            fetch(API.matches(teamName, year)).then(response => {
+                const responsePromise = response.json();
+                responsePromise.then(data => {
+                    console.log('Response ', data)
+                    setMatches(data.data);
+                });
+               
+            })
+      };
+        fetchMatch();
+    }, [])
 
     return (
-        <h3>Match Page</h3>
-
+        <React.Fragment>
+           <h1>Match Page</h1>
+           {
+             matches && matches.length &&
+             matches.map(match => <MatchDetailsCard teamName={teamName} match={match} key={match.lgId}/>)
+           }
+           </React.Fragment>    
     );
 }
 
